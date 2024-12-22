9493c1aa26ce19c4da43a52b5189414558fd613b
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2024-12-22 20:05:59 +0530

    update cls

--- blackjack.py
+++ blackjack.py
@@ -1,42 +1,77 @@
-import art
 import random
+import os
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def draw_card():
+    """Draws a random card. Ace (11), 2-10, Jack (10), Queen (10), King (10)."""
+    cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+    return random.choice(cards)
 
-print(art.logo)
+def calculate_score(cards):
+    """Calculates the score of a hand. Adjusts Ace value if score exceeds 21."""
+    score = sum(cards)
+    if 11 in cards and score > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
 
-user_cards = [random.choice(cards), random.choice(cards)]
-dealer_cards = [random.choice(cards), random.choice(cards)]
+def clear_screen():
+    """Clears the console screen."""
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-print(f"User cards: {user_cards}")
-print(f"Dealer cards: {dealer_cards}")
+def blackjack_game():
+    while True:
+        clear_screen()
+        print("Welcome to Blackjack!\n")
+        
+        # Initial card draws
+        user_cards = [draw_card(), draw_card()]
+        computer_cards = [draw_card(), draw_card()]
+        
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        
+        print(f"Your cards: {user_cards}, current score: {user_score}")
+        print(f"Computer's first card: {computer_cards[0]}")
+        
+        # Check for blackjack
+        if user_score == 21:
+            print("You have a Blackjack! You win!")
+        elif computer_score == 21:
+            print("Computer has a Blackjack! You lose.")
+        else:
+            # User's turn
+            while user_score < 21:
+                should_continue = input("Do you want to draw another card? Type 'yes' or 'no': ").lower()
+                if should_continue == 'yes':
+                    user_cards.append(draw_card())
+                    user_score = calculate_score(user_cards)
+                    print(f"Your cards: {user_cards}, current score: {user_score}")
+                else:
+                    break
+            
+            if user_score > 21:
+                print("You went over 21. You lose.")
+            else:
+                # Computer's turn
+                while computer_score < 17:
+                    computer_cards.append(draw_card())
+                    computer_score = calculate_score(computer_cards)
+                
+                print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
+                print(f"Your final hand: {user_cards}, final score: {user_score}")
+                
+                # Determine winner
+                if computer_score > 21 or user_score > computer_score:
+                    print("You win!")
+                elif user_score == computer_score:
+                    print("It's a draw!")
+                else:
+                    print("You lose!")
+        
+        # Ask if the user wants to play again
+        play_again = input("Do you want to play again? Type 'yes' or 'no': ").lower()
+        if play_again != 'yes':
+            print("Thanks for playing! Goodbye.")
+            break
 
-def calculate_score(hand):
-    score = sum(hand)
-    if score > 21 and 11 in hand:
-        hand.remove(11)
-        hand.append(1)
-        score = sum(hand)
-    return score
-
-user_score = calculate_score(user_cards)
-dealer_score = calculate_score(dealer_cards)
-
-if user_score == 21 and len(user_cards) == 2:
-    print("Blackjack! You win!")
-elif dealer_score == 21 and len(dealer_cards) == 2:
-    print("Dealer has Blackjack! You lose!")
-else:
-    print(f"User score: {user_score}")
-    print(f"Dealer score: {dealer_score}")
-
-    if user_score > 21:
-        print("Bust! You lose!")
-    elif dealer_score > 21:
-        print("Dealer busts! You win!")
-    elif user_score == dealer_score:
-        print("Draw!")
-    elif user_score > dealer_score:
-        print("You win!")
-    else:
-        print("You lose!")
+blackjack_game()
